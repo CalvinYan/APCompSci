a4a1f6e006c891133227e9b1ad8566e10b54f44a
CalvinYan <calviny524@gmail.com>
2016-08-16 21:43:03 -0700

    US flag properly displays

--- USFlag/src/FlagFrame.java
+++ USFlag/src/FlagFrame.java
@@ -3,4 +3,22 @@ import javax.swing.JFrame;
 
 public class FlagFrame extends JFrame {
 	
+	private FlagPanel panel = new FlagPanel(1330, 700);
+	
+	public FlagFrame() {
+		init();
+	}
+	
+	public void init() {
+		add(panel);
+		setResizable(true);
+		pack();
+		repaint();
+	}
+	
+	public void paint(Graphics g) {
+		panel.repaint();
+	}
+	
+	
 }
